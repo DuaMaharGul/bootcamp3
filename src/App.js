14303c84698663cb218fa107af7803731184e7ed
Dua MG <duamahargul@outlook.com>
2022-03-28 14:52:04 +0500

    update name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// all properties we pass in ReactDOM will b available in 1st arrg of func
+function App(props) {
+  return <div>Hello from Dua MG {props.name}</div>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement('div', null, "Hello World from Me"),
+ document.getElementById('root')
+);
+*/
+
+ReactDOM.render(
+   <div><App name="Dua MG"/></div>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
